ed4a222999a45fba1589ead0ed5815a53159361c
Nicole Freed <nicole.freed@gmail.com>
2017-09-19 11:24:37 -0700

    fix typo

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -11,7 +11,8 @@ export default (state = [], action) => {
           timeOpened: timeOpened,
           id: id
         }
-      default:
-        return state;
+      ]
+    default:
+      return state;
   }
 }
