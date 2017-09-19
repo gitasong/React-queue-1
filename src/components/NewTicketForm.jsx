343b68262f915db2d8890dab483f2254c4b430c0
Nicole Freed <nicole.freed@gmail.com>
2017-09-19 14:46:04 -0700

    remove console.log

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -8,7 +8,6 @@ class NewTicketForm extends React.Component {
 
   constructor(props) {
     super(props);
-    console.log(props);
     this.handleNewTicketFormSubmission = this.handleNewTicketFormSubmission.bind(this);
   }
 
