5f3a9d8ee0178080772fae6f9ed308388b635f1f
Guest User <nicole.freed@gmail.com>
2017-09-20 09:14:44 -0700

    import Admin component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Queue from "./Queue";
 import { Switch, Route } from 'react-router-dom';
+import Admin from "./Admin";
 
 function App(){
   return (
