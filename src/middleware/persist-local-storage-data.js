6ca4f4f6aad9bd4ce890cab56ed19a7380f0b343
Calla Rudolph and Nicole Freed <calla+nicole@>
2017-09-25 08:37:27 -0700

    move next() above localStorage

--- src/middleware/persist-local-storage-data.js
+++ src/middleware/persist-local-storage-data.js
@@ -1,7 +1,7 @@
 const persistDataLocally = store => next => action => {
+  next(action);
   localStorage["reduxStore"] = JSON.stringify(store.getState());
   console.log("Local Storage:", localStorage["reduxStore"]);
-  return next(action);
 }
 
 export default persistDataLocally;
